4dae07f09250bf913d4a051bdcd4c3806726e2e0
Chris Blunt <dev@chrisblunt.me>
2025-09-13 16:43:07 +0700

    test: add basic specs for failover

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,13 @@
+# Copyright 2025 Chris Blunt
+# Licensed under the Apache License, Version 2.0
+
 require "spec"
-require "../src/offgridmail-forwarder"
+
+# Require only the library parts, not the runner.
+require "../src/ogm/forwarder/config"
+require "../src/ogm/forwarder/cli"
+require "../src/ogm/forwarder/upstream_selector"
+require "../src/ogm/forwarder/session"
+require "../src/ogm/forwarder/listener"
+
+require "./support/echo_server"

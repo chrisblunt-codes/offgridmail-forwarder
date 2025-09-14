6d3350a6e97391c63cf362f2c6164168e12e0ad7
Chris Blunt <dev@chrisblunt.me>
2025-09-14 22:36:46 +0700

    Revert "test: add basic specs for failover"
    This reverts commit 4dae07f09250bf913d4a051bdcd4c3806726e2e0.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,13 +1,2 @@
-# Copyright 2025 Chris Blunt
-# Licensed under the Apache License, Version 2.0
-
 require "spec"
-
-# Require only the library parts, not the runner.
-require "../src/ogm/forwarder/config"
-require "../src/ogm/forwarder/cli"
-require "../src/ogm/forwarder/upstream_selector"
-require "../src/ogm/forwarder/session"
-require "../src/ogm/forwarder/listener"
-
-require "./support/echo_server"
+require "../src/offgridmail-forwarder"
